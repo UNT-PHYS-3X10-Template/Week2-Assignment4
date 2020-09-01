cc28d858e6be23b388fe722bef8896e6294ca076
Oliviero Andreussi <olivieroandreussi@hotmail.com>
2020-09-01 16:15:04 -0500

    Update and rename darts.py to pi_random.py

---
+++ pi_random.py
@@ -0,0 +1,10 @@
+# Week 2 Assigment 4
+# This code draws ntot points in a square of side two centered in the origin of the cartesian plane.
+# For each point, we check if it falls within the unit circle. 
+# From the ratio between the number of points within the unit circle and the total number of points we can estimate pi.
+import numpy as np
+ntot =  # yuu can 
+x,y=np.random.uniform(-1,1,2)
+if # complete the test to check if the point is within the circle
+
+print("Using %i random points, the estimate of pi is: %" %(ntot,))
